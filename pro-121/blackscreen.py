edb8e4e11ff02a8d1b2e78ddd782c6545ef4d482
PastelBees <73673440+PastelBees@users.noreply.github.com>
2021-09-05 19:48:14 -0400

    Add files via upload

---
+++ pro-121/blackscreen.py
@@ -0,0 +1,35 @@
+import cv2
+import time
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+image = cv2.imread("me.jpg")
+
+while True:
+    ret,frame = cap.read()
+    print(frame)
+
+    frame = cv2.resize(frame, (640,480))
+    image = cv2.resize(image, (640, 480))
+
+    u_black = np.array([104,153,70])
+    l_black = np.array([10,255,255])
+
+    mask = cv2.inRange(frame, l_black, u_black)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+    
+    f = frame - res
+    f = np.where(f == 0, image, f)
+
+    cv2.imshow("Cap", frame)
+    cv2.imshow("Mask", f)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
